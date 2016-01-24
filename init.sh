afc5a45bb77f235963804d58a1479ae5d80ef196
Luke Hurst <hurstlj@umich.edu>
2016-01-24 17:01:02 -0500

    Create script to automatically create symlinks

--- init.sh
+++ init.sh
@@ -1,9 +1,18 @@
 # This script configures the home directory to use shared config files
 # It creates symbolic links to all the files in this repo
 
+Ignore=('.' '..' '.git')
+
 # Remove non-shared config files
 # Create symbolic links to shared config files
 for filename in .*; do
-    rm ~/$filename;
-    ln -s $(pwd)/$filename ~/$filename;
+    # Ignore names in Ignore
+    if [[ ! " ${Ignore[@]} " =~ " $filename " ]]; then
+        echo Removing: "~"/$filename
+        rm -rf ~/$filename
+        echo Creating: "~"/$filename "-> ~/"${PWD##*/}/$filename
+        ln -s $PWD/$filename ~/$filename
+    else
+        echo Ignoring: $filename;
+    fi
 done;
