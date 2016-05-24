63020f8adb1716f0321f93ff5d870696a90b1cfc
Luke Hurst <hurstlj@umich.edu>
2016-05-23 20:08:03 -0400

    Add "yes" flag to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,7 +10,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew update && brew upgrade
 # If this is Ubuntu Linux
 elif [[ "$OSTYPE" == "linux"* ]]; then
-    sudo apt-get update && sudo apt-get upgrade
+    sudo apt-get update && sudo apt-get -y upgrade
 # If this OS isn't recognized
 else
     echo "I don't know how to update $OSTYPE"
